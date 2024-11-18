c2ad470a5fdad97b8b2a0c74813b50c23a834e62
Yawen Li <lhf552004@gmail.com>
2024-11-18 17:19:33 -0330

    update backend account name

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "terraform-backend-rg"
-    storage_account_name = "terraformstate123"
+    storage_account_name = "nlchiterraformstate123"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
